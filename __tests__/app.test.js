68e4b459ef28dfa3fd6c9e7f7a4ca9947072df1a
aidanliddiard <aidanliddiard@gmail.com>
2022-06-23 08:03:02 -0700

    settin up first test for creating a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,42 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@test.com',
+  password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await (
+    await agent.post('/api/v1/users/sessions')
+  ).setEncoding({ email, password });
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('create a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
